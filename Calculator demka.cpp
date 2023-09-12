d798c87fca99910bc0e47fb9282a9fb7910ad9e0
Vadim <56439970+Efendiev112@users.noreply.github.com>
2023-09-12 14:37:15 +0300

    Calculator no choice

---
+++ Calculator demka.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	//calculator (only output result + - * /
+	float a, b;
+	cout << "Input a ";
+	cin >> a;
+	cout << "Input b ";
+	cin >> b;
+	cout << a << "+" << b << "=" << a + b << endl;
+	cout << a << "-" << b << "=" << a - b << endl;
+	cout << a << "/" << b << "=" << a / b << endl;
+	cout << a << "*" << b << "=" << a * b << endl;
+
+ 
+}
+
+
