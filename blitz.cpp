18357a17f9e13d7314ec86555b4b21467f9cffa4
Vadim <56439970+Efendiev112@users.noreply.github.com>
2023-10-24 12:24:46 +0300

    Add files via upload

---
+++ blitz.cpp
@@ -0,0 +1,17 @@
+﻿
+
+
+
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "");
+	int a;
+	int b = 0;
+	while (a!=0)
+	{
+		cout << "Введите число";
+		cin >> b;
+	}
+	cout << b;
+}	
